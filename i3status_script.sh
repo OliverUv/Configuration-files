fae4fa7657acb422d33a248b6c81d03f502e14ec
Oliver Uvman <oliver.uvman@gmail.com>
2013-05-26 06:17:11 +0200

    Add i3 language things

---
+++ i3status_script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# prepend i3status with more stuff
+i3status --config ~/.i3/status.conf | while :
+do
+        read line
+        LG=$(setxkbmap -print | grep xkb_symbols | awk -F"+" '{print $2}') 
+        LG=$(echo $LG | sed 's/latin/en/')
+        echo "$LG | $line" || exit 1
+done
