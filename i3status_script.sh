282ea382c1488fa44c3895a15511b26cafe11d9e
Oliver Uvman <oliver.uvman@gmail.com>
2013-05-27 08:34:06 +0200

    More i3 things!

--- i3status_script.sh
+++ i3status_script.sh
@@ -6,5 +6,13 @@ do
         read line
         LG=$(setxkbmap -print | grep xkb_symbols | awk -F"+" '{print $2}') 
         LG=$(echo $LG | sed 's/latin/en/')
-        echo "$LG | $line" || exit 1
+        echo "$LG  |  $line" || exit 1
 done
+
+
+        # read line
+        # LG=$(setxkbmap -print | grep xkb_symbols | awk -F"+" '{print $2}') 
+        # LG=$(echo $LG | sed 's/latin/en/')
+        # OUT=$(echo "$LG | $line")
+        # OUT=$(echo $OUT | sed 's/|/./g')
+        # echo $OUT || exit 1
