e79513d233bfd25a807a1db2a0d1bfedd869d827
Oliver Uvman <ouvman@apple.com>
2015-04-10 19:13:45 +0200

    fish: Add env vars, use pip installed virtualfish

--- config.fish
+++ config.fish
@@ -1,8 +1,15 @@
 set PATH $PATH ~/bin
 set PATH $PATH ~/bin.priv
-set TERM screen-256color
-set fish_greeting " "
-set -x EDITOR vim
-set -x LOCATE_PATH "$HOME/.locate.db"
 
-. /home/oliver/Configuration-files/virtualfish/virtual.fish
+set fish_greeting " "
+
+set TERM screen-256color
+
+set -x LOCATE_PATH "$HOME/.locate.db"
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
+set -x EDITOR vim
+
+set -x MONGODB_HOST "localhost"
+
+eval (python -m virtualfish)
