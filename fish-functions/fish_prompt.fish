d8c0816c6ba86ed9a648d03be9f876ddcd4686b7
OliverUv <oliver.uvman@gmail.com>
2019-08-03 16:51:35 +0200

    fish: Update prompt

--- fish-functions/fish_prompt.fish
+++ fish-functions/fish_prompt.fish
@@ -1,4 +1,23 @@
 function fish_prompt --description 'Write out the prompt'
+    #Save the return status of the previous command
+    set stat $status
+
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
+
+    if not set -q __fish_color_blue
+        set -g __fish_color_blue (set_color -o blue)
+    end
+
+    #Set the color for the status depending on the value
+    set __fish_color_status (set_color -o normal)
+    set __fish_text_status " "
+    if test $stat -gt 0
+        set __fish_color_status (set_color -o red)
+        set __fish_text_status "($stat)"
+    end
+
     switch $USER
 
         case root toor
@@ -19,7 +38,7 @@ function fish_prompt --description 'Write out the prompt'
                 set -g __fish_prompt_cwd (set_color $fish_color_cwd)
             end
 
-            printf '\r\f  %s%s ➤  %s' $__fish_prompt_cwd (basename $PWD) $__fish_prompt_normal
+            printf '\r\f  %s%s%s %s %s%s ➤  %s' "$__fish_color_status" "$__fish_text_status" "$__fish_prompt_normal" (date "+%H:%M:%S") $__fish_prompt_cwd (basename $PWD) $__fish_prompt_normal
 
     end
 end
