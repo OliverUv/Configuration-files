73995076853a20d11db67296ad75a88c1aeb3ea7
OliverUv <oliver.uvman@gmail.com>
2025-05-05 18:38:59 +0200

    Delete old irrelevant configs

--- bash_aliases
+++
@@ -1,5 +0,0 @@
-alias pm='python manage.py'
-alias pmt='python manage.py test'
-alias pmr='python manage.py runserver'
-alias pms='python manage.py shell'
-alias pmsdb='python manage.py syncdb'
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DOTNAMED="profile tmux.conf Xdefaults vimrc gvimrc ctags agignore gitconfig gitignoreglobal vimperatorrc gtkrc-2.0 dmenurc taskrc xsessionrc pam_environment"
+DOTNAMED="profile tmux.conf Xdefaults vimrc gvimrc ctags gitconfig gitignoreglobal gtkrc-2.0 dmenurc taskrc xsessionrc pam_environment"
 HOME=~
 
 for f in $DOTNAMED ; do
@@ -9,7 +9,6 @@ done
 
 mkdir -p ~/.config/rua
 mkdir -p ~/.config/alacritty
-mkdir -p ~/.config/bugwarrior
 mkdir -p ~/.config/conky
 mkdir -p ~/.config/dunst
 mkdir -p ~/.config/feh
@@ -20,7 +19,6 @@ mkdir -p ~/.i3
 mkdir -p ~/.proxychains
 mkdir -p ~/.tmux/plugins
 mkdir -p ~/.urxvt/ext
-mkdir -p ~/.vimperator/info/dev-edition-default
 
 ln -s ~/Configuration-files/ranger ~/.config/
 ln -s ~/Configuration-files/systemd ~/.config/
@@ -29,12 +27,7 @@ ln -s ~/Configuration-files/jj ~/.config/
 ln -s ~/Configuration-files/alacritty.yml ~/.config/alacritty/
 ln -s ~/Configuration-files/mpv ~/.config/
 
-ln -s ~/Configuration-files/bugwarriorrc ~/.config/bugwarrior/
-
 ln -s ~/Configuration-files/rua_wrap ~/.config/rua/wrap_args.d
-
-ln -s ~/Configuration-files/coffeelint.json ~/.config/coffeelint.json
-ln -s ~/Configuration-files/flake8 ~/.config/flake8
 
 ln -s ~/Configuration-files/bin ~/bin
 
@@ -65,11 +58,6 @@ ln -s ~/Configuration-files/urxvt-perls/clipboard ~/.urxvt/ext/
 ln -s ~/Configuration-files/urxvt-perls/keyboard-select ~/.urxvt/ext/
 ln -s ~/Configuration-files/urxvt-perls/url-select ~/.urxvt/ext/
 ln -s ~/Configuration-files/urxvt-font-size/font-size ~/.urxvt/ext/
-
-ln -s ~/Configuration-files/vimperatorcolors ~/.vimperator/colors
-ln -s ~/Configuration-files/vimperatorplugin ~/.vimperator/plugin
-ln -s ~/Configuration-files/vimperatorquickmarks ~/.vimperator/info/dev-edition-default/quickmarks
-ln -s ~/Configuration-files/vimperatorignored-keys ~/.vimperator/info/dev-edition-default/ignored-keys
 
 ln -s ~/Configuration-files/i3status.conf ~/.i3/status.conf
 ln -s ~/Configuration-files/i3config.base ~/.i3/config.base
